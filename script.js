8a921db68261a1b6df96c3e9d93552ed22026d60
kalich944 <138101523+kalich944@users.noreply.github.com>
2025-04-16 23:37:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const crystals = Array.from({length: 8}, (_, i) => `c${i+1}.jpg`);
+const options = Array.from({length: 19}, (_, i) => `${i+1}.jpg`);
+
+document.getElementById('container').addEventListener('click', function() {
+  const randomCrystal = crystals[Math.floor(Math.random() * crystals.length)];
+  const randomOption = options[Math.floor(Math.random() * options.length)];
+  document.getElementById('crystal').src = randomCrystal;
+  document.getElementById('option').src = randomOption;
+});
