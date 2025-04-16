5216cbfaf048c0fd6230d6bcad040ef8175de355
kalich944 <138101523+kalich944@users.noreply.github.com>
2025-04-16 23:55:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Массивы с именами файлов кристаллов и опций
 const crystals = Array.from({length: 8}, (_, i) => `c${i+1}.jpg`);
 const options = Array.from({length: 19}, (_, i) => `${i+1}.jpg`);
 
+// При клике меняем оба изображения
 document.getElementById('container').addEventListener('click', function() {
   const randomCrystal = crystals[Math.floor(Math.random() * crystals.length)];
   const randomOption = options[Math.floor(Math.random() * options.length)];
