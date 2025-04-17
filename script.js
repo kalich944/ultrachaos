556f569c272db6ecfce7c3a32f1a45dc1f70454a
kalich944 <138101523+kalich944@users.noreply.github.com>
2025-04-17 15:04:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const crystals = Array.from({length: 8}, (_, i) => `c${i+1}.JPG`);
-const options = Array.from({length: 19}, (_, i) => `${i+1}.jpg`);
+const options = Array.from({length: 20}, (_, i) => `${i+1}.jpg`);
 
 document.getElementById('container').addEventListener('click', function() {
   const randomCrystal = crystals[Math.floor(Math.random() * crystals.length)];
