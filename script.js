3769d73af956a25b642daa36ae8de36311f6c1b3
kalich944 <138101523+kalich944@users.noreply.github.com>
2025-04-18 22:00:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 const crystals = Array.from({length: 8}, (_, i) => `c${i+1}.JPG`);
 const options = Array.from({length: 20}, (_, i) => `${i+1}.jpg`);
 
-// Инициализация Telegram Mini App
-window.Telegram.WebApp.ready();
-
-// Проверяем, принял ли пользователь условия
-const hasAcceptedTerms = localStorage.getItem('ultrachaos_terms_accepted');
-if (!hasAcceptedTerms) {
-  localStorage.setItem('ultrachaos_terms_accepted', 'true');
-  console.log('Terms accepted');
-}
-
 document.getElementById('container').addEventListener('click', function() {
   const randomCrystal = crystals[Math.floor(Math.random() * crystals.length)];
   const randomOption = options[Math.floor(Math.random() * options.length)];
