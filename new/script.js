4b700a2f17fd3447ca5fb29021b860976c239c88
kalich944 <138101523+kalich944@users.noreply.github.com>
2026-03-14 18:32:36 +0500

    Add files via upload

--- new/script.js
+++ new/script.js
@@ -1,6 +1,6 @@
 const imageContainer = document.getElementById('imageContainer');
 // Определяем максимальный номер изображения
-const maxImageNumber = 12; // Теперь 9 изображений
+const maxImageNumber = 12; // Теперь 12 изображений
 
 // Функция для добавления изображений
 function loadImages() {
@@ -8,6 +8,16 @@ function loadImages() {
     const img = document.createElement('img');
     img.src = `${i}.png`; // Путь относительный, так как изображения в той же папке
     img.alt = `Правило ${i}`;
+    
+    // Добавляем обработчик события для 11-го изображения
+    if (i === 11) {
+      img.addEventListener('click', () => {
+        window.open('https://t.me/UCrules_bot?startapp', '_blank');
+      });
+      // Меняем курсор, чтобы показать, что изображение кликабельно
+      img.style.cursor = 'pointer';
+    }
+    
     imageContainer.appendChild(img);
   }
 }
