b5ccb75acf24b1f00ec86c9dee795c66583d863e
kalich944 <138101523+kalich944@users.noreply.github.com>
2026-03-14 18:23:23 +0500

    Add files via upload

---
+++ new/script.js
@@ -0,0 +1,16 @@
+const imageContainer = document.getElementById('imageContainer');
+// Определяем максимальный номер изображения
+const maxImageNumber = 11; // Теперь 9 изображений
+
+// Функция для добавления изображений
+function loadImages() {
+  for (let i = 1; i <= maxImageNumber; i++) {
+    const img = document.createElement('img');
+    img.src = `${i}.png`; // Путь относительный, так как изображения в той же папке
+    img.alt = `Правило ${i}`;
+    imageContainer.appendChild(img);
+  }
+}
+
+// Загружаем изображения при старте
+loadImages();
